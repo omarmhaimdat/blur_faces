42fa60337b8f4caa3de5d5ae34b3136baf10ed49
Omar MHAIMDAT <omarmhaimdat@gmail.com>
2020-09-21 11:33:24 +0100

    Initial Commit

---
+++ Blur_faces/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Blur_faces
+//
+//  Created by M'haimdat omar on 21-09-2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
